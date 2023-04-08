f8fc66009304f6b77c899c87f5d90a9529d9dc52
Michal Remis <michal.remis@globallogic.com>
2023-04-08 22:04:41 +0200

    added services

---
+++ scheduler-consumer/src/main/java/com/michal/examples/scheduler/consumer/Consumer.java
@@ -0,0 +1,15 @@
+package com.michal.examples.scheduler.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Michal Remis
+ */
+@SpringBootApplication
+public class Consumer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Consumer.class, args);
+    }
+}
---
+++ scheduler-provider/src/main/java/com/michal/examples/scheduler/provider/Provider.java
@@ -0,0 +1,15 @@
+package com.michal.examples.scheduler.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Michal Remis
+ */
+@SpringBootApplication
+public class Provider {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Provider.class, args);
+    }
+}
